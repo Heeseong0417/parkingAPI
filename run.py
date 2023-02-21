6e91bec379bf182e8b85f3d233241ada2ac6697c
Heeseong0417 <101095524+Heeseong0417@users.noreply.github.com>
2023-02-21 13:42:45 +0900

    Update run.py
    ㅇ

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from socket import SocketIO
-import app
+from app import create_socket,app
 from config.config import PORT, HOST_IP
 from gevent import monkey
 #monkey.patch_all()
+application  = create_socket()
 if __name__ == "__main__":
 
   
     #app.create_flask().run(HOST_IP, port=PORT, debug=True,)
-    app.create_socket()
+    application
